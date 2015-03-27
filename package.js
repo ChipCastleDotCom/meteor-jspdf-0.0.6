94fb8267a7213ad97bf276ab7b0e06f381256183
Chip Castle <chip@chipcastle.com>
2015-03-26 19:19:24 -0500

    Update package lib for client only use

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'chipcastledotcom:meteor-jspdf',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Meteor package for wrapping jsPDF',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/ChipCastleDotCom/meteor-jspdf',
+  git: 'https://github.com/ChipCastleDotCom/meteor-jspdf.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,5 +12,5 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.5');
-  api.addFiles('lib/jspdf.min.js');
+  api.addFiles('lib/jspdf.min.js', 'client');
 });
